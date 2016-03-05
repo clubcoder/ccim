fcce4d1034db8f24a7e320215e4990305f7dd9e1
jeffreymeng <jkmexpress@yahoo.com>
2016-03-05 09:51:02 -0800

    add semicolons

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 
 if (window.location.hash === "#popout") {
-	$(".popout-hidden").hide()
-	$(".intro").html("CCIM")
-	$("#message").css('height','45%')
-	$("#text").css("height", "auto")
+	$(".popout-hidden").hide();
+	$(".intro").html("CCIM");
+	$("#message").css('height','45%');
+	$("#text").css("height", "auto");
 
 }
 
 $("#logout").click(function(){
-	var fbref = new Firebase("https://ccim2.firebaseio.com")
+	var fbref = new Firebase("https://ccim2.firebaseio.com");
 	fbref.unauth();
 	localStorage.setItem("auth", "false");
-	window.location.href = "/logout.html"
+	window.location.href = "/logout.html"; 
 })
 $(document).ready(function(){$('#message').scrollTop($('#message')[0].scrollHeight);});
 slide();
 function randoms() { // random
 	return Math.floor(Math.random() * 50 + 1);
 }
-$("#help").click(function(){$("#myModal").modal("show")})
+$("#help").click(function(){$("#myModal").modal("show");})
 function slide(){
-	var z = randoms()
+	var z = randoms();
 	$('body')
 	.css('background-image','url("'+ 'http://im.clubcoder.tk/backgrounds/' + z + '.jpg' +'")')
 	.fadeIn(300000,function(){
 		setTimeout(slide,300000);
 	});
-	$("#view-image").attr("href", 'http://im.clubcoder.tk/backgrounds/' + z + '.jpg')
+	$("#view-image").attr("href", 'http://im.clubcoder.tk/backgrounds/' + z + '.jpg');
 }
 var fb = new Firebase("https://ccim2.firebaseio.com/messages/");
 var doc = jQuery(document);
@@ -38,11 +38,11 @@ function fulltime() {
 	var hour = new Date();
 	var minute = new Date();
 	if (hour.getHours() > 12 ) {
-		var h = hour.getHours() -12
-		var a = " P.M."
+		var h = hour.getHours() -12;
+		var a = " P.M.";
 	} else {
-		var h = hour.getHours()
-		var a = " A.M."
+		var h = hour.getHours();
+		var a = " A.M.";
 	}
 	if (minute.getMinutes().length === 1) {
 		var m = "0" + minute.getMinutes();
@@ -53,7 +53,7 @@ function fulltime() {
 	return fulltime;
 }
 function setup() {
-	$('#message').scrollTop($('#message')[0].scrollHeight)
+	$('#message').scrollTop($('#message')[0].scrollHeight);
 	var button;
 	button = jQuery("#submit");
 	button.click(s);
@@ -67,7 +67,7 @@ String.prototype.replaceAll = function(search, replacement) {
 	return target.replace(new RegExp(search, 'g'), replacement);
 };
 function popout() {
-	var x = window.open('/#popout', 'test', 'status=no,location=no,toolbar=no,menubar=no,width=300,height=500')
+	var x = window.open('/#popout', 'test', 'status=no,location=no,toolbar=no,menubar=no,width=300,height=500');
 }
 function received(message) {
 	var head;
@@ -77,25 +77,25 @@ function received(message) {
 		console.log("private:" + m)
 		var command = m.substring(1, m.indexOf("/header/") );
 
-		var other = m.substring(m.indexOf("/header/") + 8, m.indexOf("/end/"))
-		var note = m.substring(m.indexOf("/end/") + 5)
-		console.log("command: " + command + " other: " + other + " note: " + note)
+		var other = m.substring(m.indexOf("/header/") + 8, m.indexOf("/end/"));
+		var note = m.substring(m.indexOf("/end/") + 5);
+		console.log("command: " + command + " other: " + other + " note: " + note);
 		if (command === "after") {
-			$('#message').scrollTop($('#message')[0].scrollHeight)
+			$('#message').scrollTop($('#message')[0].scrollHeight);
 		} else if (command === "private"){
 			if (note.indexOf(localStorage.getItem("username")) > -1 ) {
 				head.append("<p>" + note + "</p>");
 				if ($('#autoscroll').prop('checked') ) {
-					$('#message').scrollTop($('#message')[0].scrollHeight)
+					$('#message').scrollTop($('#message')[0].scrollHeight);
 
 				}
 
 			}
 		}
 	} else {
-			head.append('<p>' + m + '</p>')
+			head.append('<p>' + m + '</p>');
 			if ($('#autoscroll').prop('checked') ) {
-				$('#message').scrollTop($('#message')[0].scrollHeight)
+				$('#message').scrollTop($('#message')[0].scrollHeight);
 
 			}
 
@@ -103,18 +103,18 @@ function received(message) {
 }
 $(document).keypress(function(e) {
 	if(e.which == 13) {
-		s()
+		s();
 		return false;
 		e.preventDefualt;
 	}
 });
 function runCommand(c) {
-	var id = localStorage.getItem("username")
-	var command = c.substring(c.indexOf("/") + 1, c.indexOf("["))
-	console.log(command)
+	var id = localStorage.getItem("username");
+	var command = c.substring(c.indexOf("/") + 1, c.indexOf("["));
+	console.log(command);
 	if (command === "msg") {
-		var recipents = c.substring(c.indexOf("/msg[") + 5, c.indexOf("]"))
-		var message = c.substring(c.indexOf("] ") + 2)
+		var recipents = c.substring(c.indexOf("/msg[") + 5, c.indexOf("]"));
+		var message = c.substring(c.indexOf("] ") + 2);
 		var topush = "/private/header/" + recipents + "/end/" + fulltime() + id + " to " + recipents.replaceAll(" ", " and ").replaceAll(" i", " I").replaceAll(" j", " J").replaceAll(" d", "D").replaceAll(" l", " L") + ": " + message;
 		fb.push(topush);
 		$('#text').val('');
@@ -123,7 +123,7 @@ function runCommand(c) {
 
 }
 function s() {
-	var id = localStorage.getItem("username")
+	var id = localStorage.getItem("username");
 	var input;
 	input = jQuery("#text");
 	var message = convertToHtml(input.val().replaceAll("<", "&lt;").replaceAll(">", "&gt;") ).replaceAll("<p>", " ").replaceAll("</p>", " ");
