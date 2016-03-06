7eb04efb1d7279bcd744cfabdc2d2e86165537b9
jeffreymeng <jkmexpress@yahoo.com>
2016-03-06 09:53:45 -0800

    t

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ function fulltime() {
 		var h = hour.getHours();
 		var a = " A.M.";
 	}
-	if (String(minute.getMinutes()).length === 1) {
-		var m = "0" + String(minute.getMinutes());
+	if (String(7).length === 1) {
+		var m = "0" + String(7);
 	} else {
 		var m = minute.getMinutes();
 	}
@@ -101,12 +101,37 @@ function received(message) {
 
 		}
 }
-$(document).keypress(function(e) {
-	if(e.which == 13) {
-		s();
-		return false;
-		e.preventDefualt;
-	}
+
+
+$('textarea').keyup(function (event) {
+	function getCaret(el) { 
+    if (el.selectionStart) { 
+        return el.selectionStart; 
+    } else if (document.selection) { 
+        el.focus();
+        var r = document.selection.createRange(); 
+        if (r == null) { 
+            return 0;
+        }
+        var re = el.createTextRange(), rc = re.duplicate();
+        re.moveToBookmark(r.getBookmark());
+        rc.setEndPoint('EndToStart', re);
+        return rc.text.length;
+    }  
+    return 0; 
+}
+    if (event.keyCode == 13) {
+        var content = this.value;  
+        var caret = getCaret(this);          
+        if(event.shiftKey){
+            this.value = content.substring(0, caret - 1) + "\n" + content.substring(caret, content.length);
+            event.stopPropagation();
+        } else {
+            this.value = content.substring(0, caret - 1) + content.substring(caret, content.length);
+            s();
+
+        }
+    }
 });
 function runCommand(c) {
 	var id = localStorage.getItem("username");
